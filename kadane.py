9e8bd8d9114832e914bc2d958bfed0c484812991
PPelham <74461223+PPelham@users.noreply.github.com>
2020-11-14 11:22:17 -0500

    Update kadane.py

--- kadane.py
+++ kadane.py
@@ -1,9 +1,5 @@
-# Find the maximum subarray sum
-# Using the Kadane's Algorithm
+# Find the maximum subarray's sum using the Kadane's Algorithm
 # ie. [1, -3, 2, 1, -1] => [-3], [2, 1], [1, -3, 2, 1, -1]
-# Brute force would take O(n^2)
-# Kadane's Algorithm takes O(n)
-# Big O is Time Complexity
 
 def kadane(A):
 	max_current = max_global = A[0]
